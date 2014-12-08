56171b6491e65e6d73bc1200c02a139c6d1f1862
myming <elwin_mao@163.com>
2014-12-08 14:18:22 +0800

    ok

---
+++ walltime_tsc/c_walltime_tsc.c
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>    // for printf()  
+#include<stdlib.h>
+#include <signal.h> 
+#include <sys/time.h>
+#include <time.h>
+#include<linux/types.h> //for  __u64
+#include <errno.h>
+
+int iCnt = 0;
+__u64  val_tsc;
+struct timeval mtv;
+
+__u64 rdtsc()
+{
+        __u32 lo,hi;
+
+        __asm__ __volatile__
+        (
+         "rdtsc":"=a"(lo),"=d"(hi)
+        );
+        return (__u64)hi<<32|lo;
+}
+
+
+
+void sigFunc()
+{
+   val_tsc = rdtsc();
+   gettimeofday(&mtv, NULL);
+   printf("The %3d Times:%llu, %ld, %ld\n",iCnt++,val_tsc,mtv.tv_sec, mtv.tv_usec);
+   if(iCnt==200)
+    exit(0);
+}
+
+int main(void)
+{
+   struct itimerval tv;
+   signal(SIGALRM, sigFunc);
+   //how long to run the first time
+   tv.it_value.tv_sec = 3;
+   tv.it_value.tv_usec = 0;
+   //after the first time, how long to run next time
+   tv.it_interval.tv_sec = 0;
+   tv.it_interval.tv_usec = 10000;
+
+   if (setitimer(ITIMER_REAL, &tv, NULL) != 0)
+	printf("setitimer err %d\n", errno);
+
+   while(1)
+   {
+   }
+}
